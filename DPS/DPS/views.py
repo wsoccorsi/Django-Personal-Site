84793b7c60fc285bcabe4974a642b0582d60cdf4
wsoccorsi <wsoccorsi@gmail.com>
2019-08-19 17:40:26 -0400

    AWS update

--- DPS/DPS/views.py
+++ DPS/DPS/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('testing host again once more')
+    return render(request, 'common/view.html')
