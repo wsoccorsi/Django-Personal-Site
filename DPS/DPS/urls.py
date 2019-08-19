2cfa2cc34adcddefaf0baffa88095eb9a6f3e040
William Soccorsi <w.soccorsi@lamps.com>
2019-08-19 11:46:11 -0400

    changes realted to homepage

--- DPS/DPS/urls.py
+++ DPS/DPS/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from DPS.views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+
 ]
