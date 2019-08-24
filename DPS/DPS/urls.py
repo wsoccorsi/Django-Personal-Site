cb21997815d37d9f962e344b0b7d2954cbc9fb38
wsoccorsi <wsoccorsi@gmail.com>
2019-08-24 12:33:13 -0400

    ?

--- DPS/DPS/urls.py
+++ DPS/DPS/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from DPS.views import home, resume
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('resume/', resume, name="resume"),
