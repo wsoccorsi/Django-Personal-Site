5d89c6131b30ae5d537bc98eb4465e7b418279a8
wsoccorsi <wsoccorsi@gmail.com>
2019-08-24 12:17:46 -0400

    JS and jQuery update for large asthetic changes

--- DPS/DPS/urls.py
+++ DPS/DPS/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from DPS.views import home
+from DPS.views import home, resume
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('resume/', resume, name="resume"),
     # path('', include('api.urls')),
     path('', home),
 
--- DPS/DPS/views.py
+++ DPS/DPS/views.py
@@ -4,3 +4,6 @@ from common.models import Tech
 def home(request):
     tech_query = Tech.objects.all()
     return render(request, 'common/view.html', {'tech_stack' : tech_query})
+
+def resume(request):
+    return render(request, 'common/resume.html')
